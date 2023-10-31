369c74342659a09438754450b98d15b17366d17e
unknown <sjiff073@uottawa.ca>
2023-10-31 16:34:31 -0400

    testing complete

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ from lexical_analyzer import tokenize
 from lexical_parser import check_balanced,check_expression
 import os
 directory = r'C:\Users\Shezaad\Desktop\csi 3120\assignment2\inputs'
-for filename in os.listdir(directory):
-    print(filename)
-    filename = directory +"/"+ filename
-    
-    try:
-        tokens,error = tokenize(filename)
+# for filename in os.listdir(directory):
+# print(filename)
+filename = directory +"/"+ "input33.txt"
 
-        if (error!= ''):
-            print(error)
-            print('Syntax analysis failed.')
-        check_balanced(tokens)
-        check_expression(tokens)
-    except Exception as err:
-        err = str(err)
-        print('Syntax analysis failed')
-        if err != '':   
-            print(err)
-    else:
-        print('Syntax analysis success')
+try:
+    tokens,error = tokenize(filename)
+
+    if (error!= ''):
+        print(error)
+        print('Syntax analysis failed.')
+    check_balanced(tokens)
+    check_expression(tokens)
+except Exception as err:
+    err = str(err)
+    print('Syntax analysis failed')
+    if err != '':   
+        print(err)
+else:
+    print('Syntax analysis success')
